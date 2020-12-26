ea8ae2b2b6073782a33b952caed6ba916c75e0ec
Esther Akinloose <esther.akinloose@carpware.co>
2020-12-26 01:30:10 +0100

    updated the endpoints for LGA

--- src/lib/url.js
+++ src/lib/url.js
@@ -4,13 +4,18 @@ export const baseUrl = env().baseUrl;
 export const version = env().version;
 
 //----AUTH URLS----//
-export const login = `${baseUrl}/login`;
+export const login = `${baseUrl}/api/v${version}/login`;
 
 //----USER URLS----//
-export const users = `${baseUrl}/users`;
+export const users = `${baseUrl}/api/v${version}/users`;
 
 //----STATE URLS----//
-export const states = `${baseUrl}/states`;
+export const states = `${baseUrl}/api/v${version}/states`;
 
 //----LGA URLS----//
-export const lgas = `${baseUrl}/lgas`;
+export const allLgas = `${baseUrl}/api/v${version}/lgas`;
+export const createLga = `${baseUrl}/api/v${version}/lga`;
+export const getLgaById = `${baseUrl}/api/v${version}/lga`;
+export const getLgaByCode = `${baseUrl}/api/v${version}/lga`;
+export const updateLga = `${baseUrl}/api/v${version}/lga`;
+export const deleteLga = `${baseUrl}/api/v${version}/lga`;
