b072ecc02736ea9dfe70a6931300ee30da41554e
Esther Akinloose <esther.akinloose@carpware.co>
2021-01-01 10:17:08 +0100

    Update Party integration

--- src/Pages/Parties/UpdateParty.js
+++ src/Pages/Parties/UpdateParty.js
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import { Breadcrumbs } from 'react-breadcrumbs';
 import Layout from '../../shared/Layout';
 import {updateParty} from '../../lib/url.js';
@@ -7,20 +7,23 @@ import { showToast } from '../../helpers/showToast';
 import { PartyContext } from '../../contexts/PartyContext';
 import PartyForm from './components/Partyform';
 
-const UpdateParty = ({match, location}) => {
+const UpdateParty = ({match, location, history}) => {
     console.log(location)
     const [partyState, dispatch] = useContext(PartyContext);
+    const [party, setParty] = useState(location.state?.party);
     const handleUpdate = (values, {setSubmitting}) => {
         dispatch({type: 'UPDATE_PARTY'});
          setSubmitting(true);
-         apiRequest(updateParty, 'put', {...values})
+         apiRequest(`${updateParty}/${party.id}`, 'put', {...values})
             .then((res) => {
                 dispatch({type: 'UPDATE_PARTY_SUCCESS', payload: {response: res}});
                 setSubmitting(false);
+                history.push("/parties");
+                showToast('success', `${res.statusCode}: ${res.statusMessage}`);;
             })
             .catch((err) => {
                 dispatch({type: 'UPDATE_PARTY_FAILURE', payload: {error: err}});
-                showToast('error', 'Something went wrong. Please try again later')
+                showToast('error', `${err.response.data.statusCode? err.response.data.statusCode : ""}: ${err.response.data.statusMessage?err.response.data.statusMessage : "Something went wrong while updating party. Please try again later."}`);
                 setSubmitting(false);
             });
     }
@@ -30,7 +33,7 @@ const UpdateParty = ({match, location}) => {
                 pathname: "/parties"}, {id: 2,title: 'Update Party',
                 pathname: match.path}]}/>
             <div className="py-9 px-3.5">
-                <PartyForm handleFormSubmit={handleUpdate}/>
+                <PartyForm formFields={party} handleFormSubmit={handleUpdate}/>
             </div>
         </Layout>
     );
