f84896389cfac8f311eda619756993f0ba096886
Esther Akinloose <esther.akinloose@carpware.co>
2021-01-10 01:09:07 +0100

    Heroku build fix

--- src/config/environment/prod.config.js
+++ src/config/environment/prod.config.js
@@ -1,6 +1,6 @@
 export const production = {
   env: 'prod',
   // baseUrl: ' https://174.138.44.178:8443',
-  baseUrl: 'https://www.zaidint.ng:8080',
+  baseUrl: 'https://www.zaidint.ng',
   version: 1,
 };
