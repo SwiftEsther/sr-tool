a64c8df1eda5a56cd9ff7375160a7d89bc578c2a
Esther Akinloose <esther.akinloose@carpware.co>
2021-01-12 00:04:21 +0100

    added upload Lga url

--- src/lib/url.js
+++ src/lib/url.js
@@ -28,6 +28,7 @@ export const updateLga = `${baseUrl}/api/v${version}/lga`;
 export const deleteLga = `${baseUrl}/api/v${version}/lga/delete`;
 export const filterLgaByName = `${baseUrl}/api/v${version}/lga/filter`;
 export const getLgasByStateId = `${baseUrl}/api/v${version}/lga/state`;
+export const uploadLga = `${baseUrl}/api/v${version}/lga/upload`;
 
 //----WARD URLS----//
 export const allWards = `${baseUrl}/api/v${version}/wards`;
