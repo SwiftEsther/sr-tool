f80c6aa5879accac6a67d3e7455cec002b163478
Esther Akinloose <esther.akinloose@carpware.co>
2021-01-15 00:12:39 +0100

    Blank incident form fix

--- src/Pages/Incidents/UpdateIncident.js
+++ src/Pages/Incidents/UpdateIncident.js
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import { Breadcrumbs } from 'react-breadcrumbs';
 import Layout from '../../shared/Layout';
 import {updateResult} from '../../lib/url.js';
@@ -6,22 +6,48 @@ import {apiRequest} from '../../lib/api.js';
 import { showToast } from '../../helpers/showToast';
 import { IncidentContext } from '../../contexts/IncidentContext';
 import IncidentForm from './components/IncidentForm';
+import { values } from 'lodash';
 
-const UpdateIncident = ({match, location}) => {
-    console.log(location)
+const UpdateIncident = ({match, location, history}) => {
+    const {incident} = location.state;
+    let data = {
+        pollingUnit: incident.pollingUnit.id,
+        lga: incident.lga.id,
+        ward: incident.ward.id,
+        incidentLevel: incident.incidentLevel.id,
+        incidentType: incident.incidentType.id,
+        incidentStatus: incident.incidentStatus.id,
+        location: incident.reportedLocation,
+        phoneNumber: incident.phoneNumberToContact,
+        description: incident.description
+    };
     const [incidentState, dispatch] = useContext(IncidentContext);
+    const [currentIncident, setCurrentIncident] = useState(data);
     const handleUpdate = (values, {setSubmitting}) => {
         dispatch({type: 'UPDATE_INCIDENT'});
+        const requestBody = {
+            incidentLevelId: values.incidentLevel,
+            incidentTypeId: values.incidentType,
+            incidentStatusId: values.incidentStatus,
+            lgaId: values.lga,
+            wardId: values.ward,
+            pollingUnitId: values.pollingUnit,
+            reportedLocation: values.location,
+            phoneNumberToContact: values.phoneNumber,
+            description: values.description
+        }
          setSubmitting(true);
-         apiRequest(updateResult, 'put', {...values})
+         apiRequest(updateResult, 'put', {...requestBody})
             .then((res) => {
                 dispatch({type: 'UPDATE_INCIDENT_SUCCESS', payload: {response: res}});
                 setSubmitting(false);
+                history.push("/incidents");
+                showToast('success', `${res.statusCode}: ${res.statusMessage}`);
             })
             .catch((err) => {
                 dispatch({type: 'UPDATE_INCIDENT_FAILURE', payload: {error: err}});
-                showToast('error', 'Something went wrong. Please try again later')
                 setSubmitting(false);
+                showToast('error', `${err?.response?.data.statusCode || "Error"}: ${err?.response?.data.statusMessage || "Something went wrong. Please try again later."}`)
             });
     }
     return (
@@ -30,7 +56,7 @@ const UpdateIncident = ({match, location}) => {
                 pathname: "/incidents"}, {id: 2,title: 'Update Incident',
                 pathname: match.path}]}/>
             <div className="py-9 xl:px-3.5 px-1">
-                <IncidentForm formFields={location.state.result} handleFormSubmit={handleUpdate}/>
+                <IncidentForm formFields={currentIncident} handleFormSubmit={handleUpdate}/>
             </div>
         </Layout>
     );
