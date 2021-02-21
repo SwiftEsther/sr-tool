811e40a95ace4733320051461d8ebbe231c85780
SwiftEsther <swiftestherduro@gmail.com>
2021-02-21 21:47:56 +0100

    prod config fix

--- src/config/environment/prod.config.js
+++ src/config/environment/prod.config.js
@@ -1,6 +1,6 @@
 export const production = {
   env: 'prod',
-  // baseUrl: ' https://174.138.44.178:8443',
-  baseUrl: 'https://zaidint.ng',
+  baseUrl: 'http://174.138.44.178:80',
+  // baseUrl: 'https://zaidint.ng',
   version: 1,
 };
