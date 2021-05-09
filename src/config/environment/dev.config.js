b5d91aa215868c1413d18cdf938de2d209afdb27
SwiftEsther <swiftestherduro@gmail.com>
2021-05-09 22:30:15 +0100

    pointing app to zaiidint

--- src/config/environment/dev.config.js
+++ src/config/environment/dev.config.js
@@ -1,6 +1,7 @@
 export const dev = {
   env: 'dev',
-  baseUrl: 'http://174.138.44.178:80',
-  // baseUrl: 'https://zaidint.ng',
+  // baseUrl: 'http://174.138.44.178:8080',
+  // baseUrl: 'http://198.199.78.40',
+  baseUrl: 'https://zaidint.ng',
   version: 1,
 };
