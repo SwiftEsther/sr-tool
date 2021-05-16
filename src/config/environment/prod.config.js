a598d05590bfd289f4573e4fc5d10e01515befbc
SwiftEsther <swiftestherduro@gmail.com>
2021-05-16 23:25:45 +0100

    change baseUrl to IP address

--- src/config/environment/prod.config.js
+++ src/config/environment/prod.config.js
@@ -1,6 +1,7 @@
 export const production = {
   env: 'prod',
   // baseUrl: 'http://174.138.44.178:80',
-  baseUrl: 'https://zaidint.ng',
+  baseUrl: 'http://198.199.78.40',
+  // baseUrl: 'https://zaidint.ng',
   version: 1,
 };
